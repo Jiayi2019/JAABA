b2f741e298a7a1113481bb8077f28e59007d2aaf
Jiayi2019 <52504489+Jiayi2019@users.noreply.github.com>
2019-08-23 15:32:56 -0400

    upload read_name

---
+++ read_name.m
@@ -0,0 +1,24 @@
+%% extract information from the name
+function [driver,effector,tracker,protocol,times]=read_name(genotype)
+    index=strfind(genotype,'@');
+    driver=genotype(1:index(1)-1);
+    effector=genotype(index(1)+1:index(2)-1);
+    tracker=genotype(index(2)+1:index(3)-1);
+    protocol=genotype(index(3)+1:index(4)-1);
+    sindex=strfind(protocol,'s');
+    tindex=strfind(protocol,'x');
+    waiting=str2double(protocol(sindex(1)-2:sindex(1)-1));
+    circles=str2double(protocol(sindex(1)+1:tindex(1)-1));
+    if strcmp(protocol(tindex(1)+1:sindex(2)-1),'05')==1
+        stimdur=0.5;
+        stimint=30.5;
+    else
+        stimdur=str2double(protocol(tindex(1)+1:sindex(2)-1));
+        stimint=str2double(protocol(sindex(2)+1:sindex(3)-1));
+    end
+    times=[];
+    times.waiting=waiting;
+    times.circles=circles;
+    times.stimdur=stimdur;
+    times.stimint=stimint;
+end
