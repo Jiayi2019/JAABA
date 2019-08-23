f83e2300286212950977d3667675b2fd3e4559f0
Jiayi2019 <52504489+Jiayi2019@users.noreply.github.com>
2019-08-23 15:27:56 -0400

    modified the path finding procedure
    created find file to allow layered search of the exact file

--- import_data.m
+++ import_data.m
@@ -1,13 +1,9 @@
 %% import data file
-function data=import_data(directory,pattern)
-    %% extract data from txt files
-    Folder= dir(directory);
-    for i=1:numel(Folder)
-        if contains(Folder(i).name,pattern)
-            filename=strcat(directory,'\',Folder(i).name);
-            break
-        end
-    end
+function data=import_data(directory,marker,pattern)
+filename=find_file(directory, marker.tracker);
+filename=find_file(filename, strcat(marker.driver,'@',marker.effector));
+filename=find_file(filename, strcat(marker.protocol,'@100'));
+filename=find_file(filename,strcat(marker.driver,'@',marker.effector,'@',marker.tracker,'@',marker.protocol,'@100',pattern));
 
     %% Read columns of data as strings:
     % For more information, see the TEXTSCAN documentation.
@@ -22,4 +18,14 @@ function data=import_data(directory,pattern)
     end
     %% Convert all the numbers
     data=convert_str2num(filename,formatSpec);
+end
+
+function filename=find_file(directory,pattern)
+    Folder= dir(directory);
+    for i=1:numel(Folder)
+        if strcmp(Folder(i).name,pattern)
+            filename=strcat(directory,'\',Folder(i).name);
+            break
+        end
+    end
 end
